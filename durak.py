11e2de0d19a5a0973185f311e633a5a1df36a48b
Maxim LaPlante <144939677+TarbFela@users.noreply.github.com>
2024-07-31 14:23:16 -0400

    durak.py is now module. Main.py runs said module.

--- durak.py
+++ durak.py
@@ -2,7 +2,18 @@ import numpy as np
 from random import shuffle
 from copy import deepcopy
 from time import sleep
-card_str_dict = {0: "Hearts", 1: "Diamonds", 2: "Clubs", 3: "Spades"}
+from random import randint
+suit_str_dict = {0: "Hearts", 1: "Diamonds", 2: "Clubs", 3: "Spades"}
+DECK_SIZE_N_VALS = DSNV = 8
+
+DECK_SIZE = DS = DECK_SIZE_N_VALS * 4
+val_str_dict = { DSNV: "Ace", DSNV-1: "King", DSNV - 2: "Queen", DSNV - 3: "Jack"}
+i = 4
+v = 10
+while DSNV - i > -1:
+    val_str_dict[DSNV-i] = str(v)
+    v += -1
+    i += 1
 
 class Card:
     #suit: 0, 1, 2, 3
@@ -11,21 +22,21 @@ class Card:
         self.val = int(val)
     @property
     def mat(self):
-        mat = np.zeros([4,13],int)
+        mat = np.zeros([4,DECK_SIZE_N_VALS],int)
         mat[self.suit][self.val] = 1
             #return a matrix (suits x values)
             #with a one in the place corresponding to the card 
         return mat
     def __str__(self):
-        return f"{self.val} of {card_str_dict[self.suit]}"
+        return f"{val_str_dict[self.val]} of {suit_str_dict[self.suit]}"
     def __eq__(self,other):
         if self.suit == other.suit and self.val == other.val: return True
         else: return False
 
 def card_i(i): #get card by absolute value (cards arranged in order by value within suit
-    return Card( suit = np.floor( i / 13), val = int(i%13) )
+    return Card( suit = np.floor( i / DECK_SIZE_N_VALS), val = int(i%DECK_SIZE_N_VALS) )
 def i_card(card): #get absolute value of card
-    return 13*card.suit + card.val
+    return DECK_SIZE_N_VALS*card.suit + card.val
 #it's really just a fancy list...
 class Hand: #it holds cards. The deck is a hand. Piles are hands. Players are hands.
     def __init__(self, deck = None, name = "HAND"):
@@ -53,15 +64,27 @@ class Hand: #it holds cards. The deck is a hand. Piles are hands. Players are ha
         for card in other_hand: new_hand.append(card)
         #print(f"adding result: {new_hand}\n... ...")
         return new_hand
-    
+    def sort(self, key = "suit"):
+        cp = self.cards
+        if key == "suit":
+            cp = [(c.suit, c.val) for c in cp]
+            cp.sort()
+            cp = [ Card(s,v) for s,v in cp ]
+        else:
+            cp = [(c.val, c.suit) for c in cp]
+            cp.sort()
+            cp = [ Card(s,v) for v,s in cp ]
+        self.cards = cp
+        
     @property
     def n_cards(self):
         return len(self.cards)
     def __bool__(self):
         return len(self.cards) != 0
     
+    
     @property
-    def mat(self): #a 4x13 matrix of all cards, with a 1 in each index for each card in hand
+    def mat(self): #a 4xDECK_SIZE_N_VALS matrix of all cards, with a 1 in each index for each card in hand
         if self.n_cards > 1:
             mat = np.sum([card.mat for card in self], 0) #each card has a matrix and each card is unique
             # by virtue of the deck. The sum will be all zeroes and ones.
@@ -70,7 +93,7 @@ class Hand: #it holds cards. The deck is a hand. Piles are hands. Players are ha
             mat = self[0].mat
             return mat
         else:
-            return np.zeros([4,13],int) #no cards
+            return np.zeros([4,DECK_SIZE_N_VALS],int) #no cards
             
     def __str__(self): #just a way to print the hand out
         s= f"{self.name}: {self.n_cards} cards"
@@ -84,7 +107,7 @@ class Hand: #it holds cards. The deck is a hand. Piles are hands. Players are ha
         #print(possibility_vect)
         #print("DECIDING HAND", self)
         decision = self.decision_process(possibility_vect)
-        if decision == 52: return "END"
+        if decision == DECK_SIZE: return "END"
         decision = card_i(decision)
         print("DECISION", decision)
         self.remove(decision) #play the card and take it out of hand
@@ -101,15 +124,16 @@ class Hand: #it holds cards. The deck is a hand. Piles are hands. Players are ha
 
 class Human_Player(Hand):
     def decision_process(self, possibility_vect):
+        self.sort() #convenience...
         v = possibility_vect
         print(np.sum(v[:-1]))
         if np.sum(v[:-1]) == 0:
-            sleep(1)
-            print("DEFAULT END TURN / FORFEIT")
-            sleep(1)
-            return 52
+            sleep(0.2)
+            print("automatic end of turn: no further options")
+            sleep(0.2)
+            return DECK_SIZE
         c_list = [] #list of possible cards from hand and possibility mask
-        for i, val in enumerate(v[:-1]): #ignore 52 "FORFEIT"
+        for i, val in enumerate(v[:-1]): #ignore DECK_SIZE "FORFEIT"
             if val: c_list.append(card_i(i)) # if card is possible, get the corresponding Card object
         c_dict = {} #dictionary of possible inputs vs return values (ints)
         print("\tYOUR HAND:")
@@ -122,24 +146,24 @@ class Human_Player(Hand):
             else: nstr = ' • '
             print(f"\t{nstr}\t{card}")
         print(f"\t({i})\tEND TURN / FORFEIT")
-        c_dict[i] = 52 #don't forget the forfeit option!
+        c_dict[i] = DECK_SIZE #don't forget the forfeit option!
         return c_dict[int(input("INPUT OPTION #: "))]
         
     def decision_process_old(self, possibility_vect):
         print(self)
         v = possibility_vect
         for i, val in enumerate(v):
-            if i == 52:
-                print("52: FORFEIT")
+            if i == DECK_SIZE:
+                print("DECK_SIZE: FORFEIT")
                 break
             if val:
                 print(f"{i}: {card_i(i)}") #show the option
-        return int(input("INPUT: ")) #return an int which will be passed to card_i or will be 52 = "END"
+        return int(input("INPUT: ")) #return an int which will be passed to card_i or will be DECK_SIZE = "END"
         
         
 class Deck(Hand):
     def __init__(self):
-        self.cards = [ Card(s,v) for s in range(4) for v in range(13) ]
+        self.cards = [ Card(s,v) for s in range(4) for v in range(DECK_SIZE_N_VALS) ]
         shuffle(self.cards)
         self.name = "DECK"
     def draw(self): #take from the top
@@ -161,11 +185,11 @@ class Round:
         print("\n\n\t\t\tROUND START")#,self.a, "\n", self.d)
         
     def start(self):
-        sleep(1)
+        sleep(0.2)
         while True: #until return 
             print("\t\t\tATTACKING")
             self.do_attack()
-            sleep(1)
+            sleep(0.2)
             
             if self.open_attacks.n_cards == 0: #if no cards are played that implies a forfeit
                 return self.hands, "ATTACK FORFEIT"
@@ -174,7 +198,7 @@ class Round:
             
             while self.open_attacks.n_cards > 0: #while there are open attacks...
                 print(self.open_attacks)
-                sleep(1)
+                sleep(0.2)
                 if self.do_defend() == "DEFENCE FORFEIT": #pick up the cards!
                     self.d = self.d + self.open_attacks
                     self.d = self.d + self.discard_pile
@@ -192,7 +216,7 @@ class Round:
     def do_attack(self):
         #limit cards played according to size of other guy's hand
         while self.d.n_cards > self.open_attacks.n_cards and bool(self.a): #play as many cards as you wish...
-            sleep(1)
+            sleep(0.2)
             #get attacker's choice. Pass the constraints from the round information
             a_card = self.a.attack(self.attack_possibilities_mat())
             if type(a_card) == Card: #if a card is played, put it into the attacks section
@@ -202,7 +226,7 @@ class Round:
         if self.a: return "DONE" #if attacker still has cards, all is well
         else: return "OUT" #if attacker is out of cards, take note
     def do_defend(self):
-        sleep(1)
+        sleep(0.2)
         #get defender's choice. Pass the constraints from the round information
         d_card = self.d.defend(self.defence_possibilities_mat()) 
         if type(d_card) == str: #if it's not a card, it's a forfeit
@@ -217,10 +241,10 @@ class Round:
     
     def defence_possibilities_mat(self):
         card = self.open_attacks[0]
-        mat = np.zeros([4,13],int) #no options
+        mat = np.zeros([4,DECK_SIZE_N_VALS],int) #no options
         if card.suit != self.trump_suit:
             mat[self.trump_suit] = 1 #if the attacking card is not a trump, all trumps are better
-        mat[card.suit][card.val + 1 : 13] = 1 #all cards of same suit and higher value are better
+        mat[card.suit][card.val + 1 : DECK_SIZE_N_VALS] = 1 #all cards of same suit and higher value are better
         return mat
     
     def attack_possibilities_mat(self):
@@ -230,7 +254,7 @@ class Round:
             #note: the addition would be a problem if cards are repeated.
             #They shouldn't be, since they are unique by virtue of the deck
             in_play_mat = (self.open_attacks + self.discard_pile).mat
-        else: return np.ones([4,13],int) #if no cards played, any card is good
+        else: return np.ones([4,DECK_SIZE_N_VALS],int) #if no cards played, any card is good
 
         m = deepcopy(in_play_mat)
         #print(m)
@@ -248,9 +272,9 @@ class Game:
         self.deck = Deck()
         self.trump_card = self.deck.cards[0] #bottom card is trump
         self.trump_suit = self.trump_card.suit
-        global card_str_dict
-        card_str_dict = {0: "Hearts", 1: "Diamonds", 2: "Clubs", 3: "Spades"} #reset the printout info
-        card_str_dict[self.trump_suit] += "*" #modify the printout info
+        global suit_str_dict
+        suit_str_dict = {0: "Hearts", 1: "Diamonds", 2: "Clubs", 3: "Spades"} #reset the printout info
+        suit_str_dict[self.trump_suit] += "*" #modify the printout info
         self.players = [Hand(self.deck), Human_Player(self.deck)]
 
         print(f"Trump Card: {self.trump_card}")
@@ -260,9 +284,9 @@ class Game:
 
         hands, result = Round( self.players[i], self.players[1-i], self.trump_card.suit).start()
         self.players[i], self.players[1-i] = hands #hard update on hands
-        sleep(1)
+        sleep(0.2)
         print(result)
-        sleep(1)
+        sleep(0.2)
         #print out player hands... don't do this if you're playing fair
         #for player in self.players: print("END ROUND:", player)
 
@@ -275,10 +299,10 @@ class Game:
         # (innaccurate) win conditions...   
         if self.players[i].n_cards == 0:
             print(f"PLAYER {i} WON! \n{self.players[i]}")
-            return str(i)
+            return i
         elif self.players[1-i].n_cards == 0:
             print(f"PLAYER {1-i} WON! \n{self.players[1-i]}")
-            return str(1-i)
+            return 1-i
         if result == "DEFENCE FORFEIT": return "ROUND LOST"
         return "CONTINUE"
 
@@ -289,11 +313,19 @@ class Game:
             if do_round_message != "ROUND LOST": i = 1 - i
             print(f"deck has {self.deck.n_cards} cards")
             do_round_message = self.do_round(i)
+        if input("\n\n\t\t\tGAME OVER.\nEnter 'y' to play again!") == 'y':
+            Play()
+            return
+        print("\n\nGoodbye!")
+        sleep(2)
+        
     
     
+def Play():
+    g = Game()
+    g.do_game(randint(0,1))
 
-### Actual code is only two lines!
+    
+### Actual code is only one line!
             
-g = Game()
-#for player in g.players: print(player)
-g.do_game(0)
+#Play()
---
+++ main.py
@@ -0,0 +1,3 @@
+from durak import *
+
+Play()
